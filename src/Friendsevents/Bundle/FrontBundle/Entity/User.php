efe7f0da50db5acf7d4688fb848a36f9a4022d66
Juanito <jeancliford.charlemagne@gmail.com>
2015-06-07 17:08:06 +0200

    [Tech] FOS UserBundle installation

---
+++ src/Friendsevents/Bundle/FrontBundle/Entity/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Friendsevents\Bundle\FrontBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Entity\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="frev_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
